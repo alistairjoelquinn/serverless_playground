6399cfc6b6021ffa9cb050b18ab93a7463cabe1a
alistairjoelquinn <alistairjoelquinn@gmail.com>
2020-06-25 17:32:28 +0200

    import m-ui to experiment with contenets

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 
 export default function App() {
     return (
         <div style={styles.main}>
-            <div>Al's online playground</div>
+            <div style={styles.margin}>Al's online playground</div>
+            <Button variant="contained">
+                Big button
+            </Button>
         </div>
     );
 };
@@ -19,5 +23,8 @@ const styles = {
         width: '100vw',
         backgroundColor: 'dimgrey',
         color: 'antiquewhite'
+    },
+    margin: {
+        margin: 20
     }
-}
+};
