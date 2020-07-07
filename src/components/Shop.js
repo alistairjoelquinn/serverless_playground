574e61b09df61b07f15fb056e719e948fe668376
alistairjoelquinn <alistairjoelquinn@gmail.com>
2020-07-07 17:29:50 +0200

    shop button links correctly, plus div formatting complete

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { Typography, Grid, Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import Colors from '../../constants/Colors';
 
 const useStyles = makeStyles({
     button: {
         color: 'tomato'
     },
-    shopContainer: {
-        marginTop: 50
+    bannerText: {
+        width: '100%',
+        marginTop: 50,
+        color: Colors.paleWhite,
+        marginBottom: 50
+    },
+    background: {
+        backgroundColor: Colors.grey,
+        height: '100vh',
+        paddingTop: 20,
+        paddingBottom: 20
     }
 });
 
@@ -15,11 +25,25 @@ const Shop = props => {
     const classes = useStyles();
 
     return (
-        <Grid container direction='column'>
-            <Grid item container className={classes.shopContainer}>
+        <Grid container direction='column' className={classes.background}>
+            <Grid item container>
+                <Grid item sm={2}/>
+                <Grid item sm={8}>
+                    <Typography variant='h4' color='white' className={classes.bannerText}>This is the place to buy things. Click on the button.</Typography>
+                </Grid>
+                <Grid item sm={2}/>
+            </Grid>
+            <Grid item container>
                 <Grid item sm={5}/>
                 <Grid item sm={2}>
-                    <Button variant='contained' className={classes.button}>Buy Things</Button>
+                    <Button 
+                        variant='contained' 
+                        className={classes.button}
+                        href='https://www.youtube.com/watch?v=SRcnnId15BA'
+                        target='_blank'
+                    >
+                        Buy Things
+                    </Button>
                 </Grid>
                 <Grid item sm={5}/>
             </Grid>
