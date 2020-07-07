50e0f477451ab60101273e2ee74964b72bb30e85
alistairjoelquinn <alistairjoelquinn@gmail.com>
2020-07-07 15:45:01 +0200

    shop component now has button sitting nicely in middle

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import { Typography, Grid } from '@material-ui/core';
+import { Typography, Grid, Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    button: {
+        color: 'tomato'
+    },
+    shopContainer: {
+        marginTop: 50
+    }
+});
 
 const Shop = props => {
+    const classes = useStyles();
+
     return (
-        <Grid container>
-            <Grid item>
-                <Typography>
-                    Shop
-                </Typography>
+        <Grid container direction='column'>
+            <Grid item container className={classes.shopContainer}>
+                <Grid item sm={5}/>
+                <Grid item sm={2}>
+                    <Button variant='contained' className={classes.button}>Buy Things</Button>
+                </Grid>
+                <Grid item sm={5}/>
             </Grid>
         </Grid>
     );
