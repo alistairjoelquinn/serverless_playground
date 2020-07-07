1f79952ca5451debb84fbc43bced6eb62099f957
alistairjoelquinn <alistairjoelquinn@gmail.com>
2020-07-07 15:20:03 +0200

    linkedin link added to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { AppBar, Toolbar, Typography, makeStyles, IconButton } from '@material-ui/core';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import { NavLink } from 'react-router-dom';
 import Colors from '../../constants/Colors';
 
 const useStyles = makeStyles({
-    typographyStyles: {
+    headerLinkStyles: {
         flex: 1,
         textDecoration: 'none',
-        color: 'primary'
+        color: Colors.reactBlue
     }
 });
 
@@ -21,13 +22,18 @@ const Header = props => {
             <Toolbar>
                 <NavLink 
                     to='/' 
-                    className={classes.typographyStyles}
+                    className={classes.headerLinkStyles}
                     activeStyle={{color: Colors.reactBlue}}
                 >
                     <Typography> 
                         Alistair Quinn
                     </Typography>
                 </NavLink>
+                <a href="https://www.linkedin.com/in/alistair-quinn-5b8878199/" target="_blank">
+                    <IconButton>
+                        <LinkedInIcon />
+                    </IconButton>
+                </a>
                 <a href="https://github.com/alistairjoelquinn" target="_blank">
                     <IconButton>
                         <GitHubIcon />
