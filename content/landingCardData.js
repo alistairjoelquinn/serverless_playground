a8d3f72da96fdae671aa8f90e93c339d4e883ee3
alistairjoelquinn <alistairjoelquinn@gmail.com>
2020-07-08 10:21:24 +0200

    landing cards generated dynamically from content file

---
+++ content/landingCardData.js
@@ -0,0 +1,13 @@
+export default [
+    {
+        datePosted: "5th July, 2020",
+        w1: 'al',
+        w2: 'is',
+        w3: 'tair',
+        w4: 'quinn',
+        wordType: 'noun',
+        definition: 'well meaning and lovely.',
+        example: '"a benevolent smile"',
+        learnMoreLink: "https://github.com/alistairjoelquinn"
+    }
+];
--- src/components/LandingPageContent.js
+++ src/components/LandingPageContent.js
@@ -2,37 +2,14 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 import LandingCard from './LandingCard';
 
+import cardData from '../../content/landingCardData'
+
 const LandingPageContent = () => {
     return (
         <Grid container direction='column' spacing={4}>
             <Grid item xs={12} sm={8}>
-                <LandingCard 
-                    datePosted="5th July, 2020"
-                    w1='al'
-                    w2='is'
-                    w3='tair'
-                    w4='quinn'
-                    wordType="noun"
-                    definition='well meaning and lovely.'
-                    example='"a benevolent smile"'
-                    learnMoreLink="https://github.com/alistairjoelquinn"
-                />
+                {cardData.map(data => <LandingCard {...data} />)}
             </Grid>
-            {/* <Grid item xs={12} sm={8}>
-                <LandingCard 
-                    datePosted="3th July, 2020"
-                />
-            </Grid>
-            <Grid item xs={12} sm={8}>
-                <LandingCard 
-                    datePosted="20th June, 2020"
-                />
-            </Grid>
-            <Grid item xs={12} sm={8}>
-                <LandingCard 
-                    datePosted="4th April, 2020"
-                />
-            </Grid> */}
         </Grid>
     );
 };
