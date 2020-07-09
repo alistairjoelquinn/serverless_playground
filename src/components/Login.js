1eb7872ac0b597012548a14eefcbec6d32cecbc4
alistairjoelquinn <alistairjoelquinn@gmail.com>
2020-07-09 17:44:36 +0200

    login page input fields added

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,22 +1,85 @@
 import React from 'react';
-import { Typography, Paper } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles'
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import LockOpenIcon from '@material-ui/icons/LockOpen';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Colors from '../../constants/Colors';
 
-const useStyles = makeStyles({
-    screen: {
-        height: '100vh',
-        width: '100vw'
-    }
-});
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%',
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+        color: Colors.paleWhite
+    },
+}));
 
-const Login = props => {
+const Login = () => {
     const classes = useStyles();
 
     return (
-        <Paper className={classes.screen} square>
-            <Typography>Login</Typography>
-        </Paper>
+        <Container component="main" maxWidth="xs">
+        <CssBaseline />
+            <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <LockOpenIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Hello Handsome
+                </Typography>
+                <form className={classes.form} noValidate>
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                    />
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                    />
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                        onClick={e => e.preventDefault()}
+                    >
+                        Sign In
+                    </Button>
+                </form>
+            </div>
+        </Container>
     );
-};
+}
 
 export default Login;
